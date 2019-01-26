c3eaad60c481212cc923dbca5617bf3d9aab0594
qNickq <qNickq@bk.ru>
2019-01-26 04:04:36 +0300

    Update detecting enemies

--- tower.cpp
+++ tower.cpp
@@ -6,12 +6,17 @@ Tower::Tower(QGraphicsObject *parent)
     cost[0] = 50; cost[1] = 100; cost[2] = 200;
     power[0] = 20; power[1] = 40; power[2] = 70;
     radius[0] = 200; radius[1] = 250; radius[2] = 300;
+
     area = nullptr;
+
     setAcceptHoverEvents(true);
+
     sprite = new QPixmap(":/res/images/Tower.png");
     setFlag(QGraphicsItem::ItemIsSelectable);
+
     l_centerX = sprite->width()/2;
     l_centerY = sprite->height()/2;
+
     detecter = new QTimer();
     connect(detecter, SIGNAL(timeout()), this, SLOT(chooseEnemy()));
     detecter->start(1000);
@@ -26,25 +31,26 @@ void Tower::setArea()
 
 void Tower::chooseEnemy()
 {
-    detected = false;
-    point = 0;
     QList<QGraphicsItem *> colliding_items = area->collidingItems(Qt::IntersectsItemShape);
+
+    int max = 0;
 
     for (int i = 0, n = colliding_items.size(); i < n; ++i)
     {
-        Enemy * enemy = dynamic_cast<Enemy *>(colliding_items[i]);
-        if(enemy)
+        Enemy *enemy = dynamic_cast<Enemy *>(colliding_items[i]);
+
+        if(enemy && enemy->getDistance() > max)
         {
-            if(enemy->getPoint() > point)
-            {
-                enemyPlace = QPointF(enemy->pos());
-                point = enemy->getPoint();
-                target = enemy;
-                detected = true;
-            }
+            max = enemy->getDistance();
+            target = enemy;
         }
     }
-    if(detected) fire();
+
+    if(max)
+    {
+        enemyPlace = QPointF(target->pos());
+        fire();
+    }
 }
 
 void Tower::upgrade()
@@ -86,9 +92,11 @@ void Tower::fire()
 {
     Bullet* bullet = new Bullet(this);
     bullet->setPos(centerX, centerY);
+
     QLineF ln(QPointF(centerX-l_centerX,centerY-l_centerY), enemyPlace);
     double angle = -1*ln.angle();
     bullet->setRotation(angle);
+
     scene()->addItem(bullet);
     target->damaged(power[level]);
 }
